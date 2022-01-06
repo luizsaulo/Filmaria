3aa85cb2f31a17ff39ea6a158803fd30ed5152cf
luizsaulo <luizsaulo@yahoo.com.br>
2022-01-05 23:26:05 -0300

    criando a página Filme

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,36 @@
+import { useEffect, useState} from 'react';
+import './filme-info.css';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+
+export default function Filme() {
+    const { id } = useParams();
+    const [filme, setFilme] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+
+        async function loadFilme(){
+            const response = await api.get(`r-api/?api=filmes/${id}`);
+            setFilme(response.data);
+            setLoading(false);
+        }
+
+        loadFilme();
+
+    }, [id]);
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carregando seu filme...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className='filme-info'>
+            <h1>PAGINA DE DETALHES - {id} </h1>
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import Header from './components/Header';
 
 import Home from './pages/Home';
 
+import Filme from './pages/Filme';
+
 const Routes = () => {
   return(
     <BrowserRouter>
       <Header/>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/filme/:id" component={Filme} />
       </Switch>
     </BrowserRouter>
   )
